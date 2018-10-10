04db4502241d4a7b5a7049dce30dff25a409a179
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2018-10-10 14:55:58 +0200

    changed Benchmarks to be more generic

--- src/test/java/com/goekay/Benchmarks.java
+++ src/test/java/com/goekay/Benchmarks.java
@@ -1,17 +1,25 @@
 package com.goekay;
 
+import com.example.myproto.Protobuf;
 import com.example.myschema.ArrayOfBeer;
-import com.goekay.json.JacksonJsonStringMapper;
+import com.goekay.conversion.protobuf.ProtobufConverter;
+import com.goekay.json.JacksonJsonByteArrayMapper;
 import com.goekay.msgpack.MessagePackByteArrayMapper;
-import com.goekay.xml.JacksonXmlStringMapper;
+import com.goekay.protobuf.ProtobufByteArrayMapper;
+import com.goekay.xml.JacksonXmlByteArrayMapper;
+import com.goekay.xml.JaxbXmlByteArrayMapper;
 import com.goekay.xml.JaxbXmlStringMapper;
+import lombok.RequiredArgsConstructor;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -23,110 +31,92 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 /**
- * @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
- * @since 09.10.2018
+ * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
  */
-@State(Scope.Benchmark)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class Benchmarks {
+    private static final String xmlFile = readFile(Paths.get("src/main/resources", "beers.xml"));
+    private static final ArrayOfBeer groundTruth = new JaxbXmlStringMapper(false).readNoThrow(xmlFile);
 
-    private JacksonJsonStringMapper jacksonJsonMapper;
+    private static String readFile(final Path path) {
+        final byte[] encoded;
+        try {
+            encoded = Files.readAllBytes(path);
+        } catch (IOException e) {
+            throw new Error(e);
+        }
+        return new String(encoded, StandardCharsets.UTF_8);
+    }
 
-    private JaxbXmlStringMapper jaxbXmlMapper;
-    private JacksonXmlStringMapper jacksonXmlMapper;
+    @BenchmarkMode(Mode.AverageTime)
+    @Fork(1)
+    @State(Scope.Thread)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+    @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+    @RequiredArgsConstructor
+    public static abstract class AbstractBenchmark<BASEMODEL> {
+        final String fileNameAndSuffix;
+        final Function<ArrayOfBeer, BASEMODEL> converter;
+        final ByteArrayMapper<BASEMODEL> mapper;
+        BASEMODEL model;
+        byte[] bytes;
 
-    private MessagePackByteArrayMapper messagePackMapper;
+        @Setup
+        public void setup() {
+            model = converter.apply(groundTruth);
+            bytes = mapper.writeNoThrow(model);
+        }
 
-    private String dataAsXmlString;
-    private String dataAsJsonString;
-    private byte[] dataAsMsgPack;
+        @Benchmark
+        public void benchToObject(final Blackhole bh) {
+            bh.consume(mapper.readNoThrow(bytes));
+        }
 
-    private ArrayOfBeer dataAsObject;
+        @Benchmark
+        public void benchToByteArray(final Blackhole bh) {
+            bh.consume(mapper.writeNoThrow(model));
+        }
+    }
 
-    /**
-     * Main method to run benchmark
-     */
+    public static class JaxbXmlBenchmark extends AbstractBenchmark<ArrayOfBeer> {
+        public JaxbXmlBenchmark() {
+            super("jaxb.xml", Function.identity(), new JaxbXmlByteArrayMapper(false));
+        }
+    }
+
+    public static class JacksonXmlBenchmark extends AbstractBenchmark<ArrayOfBeer> {
+        public JacksonXmlBenchmark() {
+            super("jackson.xml", Function.identity(), new JacksonXmlByteArrayMapper());
+        }
+    }
+
+    public static class JacksonJsonBenchmark extends AbstractBenchmark<ArrayOfBeer> {
+        public JacksonJsonBenchmark() {
+            super("jackson.json", Function.identity(), new JacksonJsonByteArrayMapper());
+        }
+    }
+
+    public static class MessagePackBenchmark extends AbstractBenchmark<ArrayOfBeer> {
+        public MessagePackBenchmark() {
+            super("msgpack", Function.identity(), new MessagePackByteArrayMapper());
+        }
+    }
+
+    public static class ProtobufBenchmark extends AbstractBenchmark<Protobuf.ArrayOfBeerType> {
+        public ProtobufBenchmark() {
+            super("protobuf", ProtobufConverter.INSTANCE::convert, ProtobufByteArrayMapper.INSTANCE);
+        }
+    }
+
     public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder().include(Benchmarks.class.getSimpleName())
-                                          .warmupIterations(4)
-                                          .measurementIterations(4)
-                                          .forks(1)
-                                          .threads(1)
-                                          .build();
+        Options opt = new OptionsBuilder()
+                .include(Benchmarks.class.getSimpleName())
+                .build();
 
         new Runner(opt).run();
-    }
-
-    @Setup
-    public final void prepare() throws Exception {
-        String tmp = readFile(Paths.get("src/main/resources", "beers.xml"));
-
-        jaxbXmlMapper = new JaxbXmlStringMapper(false);
-        jacksonJsonMapper = new JacksonJsonStringMapper();
-        jacksonXmlMapper = new JacksonXmlStringMapper();
-        messagePackMapper = new MessagePackByteArrayMapper();
-
-        dataAsObject = jaxbXmlMapper.readNoThrow(tmp);
-        dataAsXmlString = jaxbXmlMapper.writeNoThrow(dataAsObject);
-        dataAsMsgPack = messagePackMapper.write(dataAsObject);
-
-        dataAsJsonString = jacksonJsonMapper.writeNoThrow(dataAsObject);
-        ArrayOfBeer dataAsObject2 = jacksonJsonMapper.readNoThrow(dataAsJsonString);
-        ArrayOfBeer dataAsObject3 = messagePackMapper.readNoThrow(dataAsMsgPack);
-
-        Files.write(Paths.get("target", "data-jaxb.xml"), dataAsXmlString.getBytes(), StandardOpenOption.CREATE);
-        Files.write(Paths.get("target", "data-jackson.json"), dataAsJsonString.getBytes(), StandardOpenOption.CREATE);
-        Files.write(Paths.get("target", "data-jackson.xml"), jacksonXmlMapper.writeNoThrow(dataAsObject2).getBytes(), StandardOpenOption.CREATE);
-        Files.write(Paths.get("target", "data-msgpack"), messagePackMapper.writeNoThrow(dataAsObject3), StandardOpenOption.CREATE);
-    }
-
-    @Benchmark
-    public void jaxbParserToObject(Blackhole bh) {
-        bh.consume(jaxbXmlMapper.readNoThrow(dataAsXmlString));
-    }
-
-    @Benchmark
-    public void jaxbParserToString(Blackhole bh) {
-        bh.consume(jaxbXmlMapper.writeNoThrow(dataAsObject));
-    }
-
-    @Benchmark
-    public void jacksonXmlMapperToObject(Blackhole bh) {
-        bh.consume(jacksonXmlMapper.readNoThrow(dataAsXmlString));
-    }
-
-    @Benchmark
-    public void jacksonXmlMapperToString(Blackhole bh) {
-        bh.consume(jacksonXmlMapper.writeNoThrow(dataAsObject));
-    }
-
-    @Benchmark
-    public void jacksonJsonMapperToObject(Blackhole bh) {
-        bh.consume(jacksonJsonMapper.readNoThrow(dataAsJsonString));
-    }
-
-    @Benchmark
-    public void jacksonJsonMapperToString(Blackhole bh) {
-        bh.consume(jacksonJsonMapper.writeNoThrow(dataAsObject));
-    }
-
-    @Benchmark
-    public void msgPackToObject(Blackhole bh) {
-        bh.consume(messagePackMapper.readNoThrow(dataAsMsgPack));
-    }
-
-    @Benchmark
-    public void msgPackToByteArray(Blackhole bh) {
-        bh.consume(messagePackMapper.writeNoThrow(dataAsObject));
-    }
-
-    private String readFile(Path path) throws IOException {
-        byte[] encoded = Files.readAllBytes(path);
-        return new String(encoded, StandardCharsets.UTF_8);
     }
 }
