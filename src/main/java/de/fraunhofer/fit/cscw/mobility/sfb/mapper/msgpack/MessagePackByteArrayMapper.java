4df3d36c8fa461a48436f772ceece8db34a91841
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2018-10-10 17:10:48 +0200

    change config for reduced msg pack data size

--- src/main/java/de/fraunhofer/fit/cscw/mobility/sfb/mapper/msgpack/MessagePackByteArrayMapper.java
+++ src/main/java/de/fraunhofer/fit/cscw/mobility/sfb/mapper/msgpack/MessagePackByteArrayMapper.java
@@ -4,6 +4,7 @@ import com.example.myschema.ArrayOfBeer;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.fraunhofer.fit.cscw.mobility.sfb.mapper.ByteArrayMapper;
+import org.msgpack.jackson.dataformat.JsonArrayFormat;
 import org.msgpack.jackson.dataformat.MessagePackFactory;
 
 /**
@@ -16,6 +17,11 @@ public class MessagePackByteArrayMapper implements ByteArrayMapper<ArrayOfBeer> 
 
     public MessagePackByteArrayMapper() {
         mapper = new ObjectMapper(new MessagePackFactory());
+
+        // this config greatly reduces the final size, but might cause problems!
+        // https://github.com/msgpack/msgpack-java/blob/develop/msgpack-jackson/README.md#serializedeserialize-pojo-as-messagepack-array-type-to-keep-compatibility-with-msgpack-java06
+        mapper.setAnnotationIntrospector(new JsonArrayFormat());
+
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
