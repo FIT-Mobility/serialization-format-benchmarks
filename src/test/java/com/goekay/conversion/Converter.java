8de06e9ce73c37f31dbc5c9aafae6ca3036760cf
Fabian Ohler <fabian.ohler1@rwth-aachen.de>
2018-10-10 14:53:36 +0200

    refactor

--- src/test/java/com/goekay/conversion/Converter.java
+++ src/test/java/com/goekay/conversion/Converter.java
@@ -3,6 +3,7 @@ package com.goekay.conversion;
 /**
  * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
  */
+@FunctionalInterface
 public interface Converter<BASEMODEL, POJO> {
     POJO convert(BASEMODEL basemodel);
 }
