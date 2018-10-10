add75399b7a62aa253976ad3c736a06dfa1cbb77
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2018-10-10 15:59:53 +0200

    SanityChecks: write to file before tests

--- src/test/java/de/fraunhofer/fit/cscw/mobility/sfb/SanityChecks.java
+++ src/test/java/de/fraunhofer/fit/cscw/mobility/sfb/SanityChecks.java
@@ -6,8 +6,13 @@ import de.fraunhofer.fit.cscw.mobility.sfb.json.JacksonJsonStringMapper;
 import de.fraunhofer.fit.cscw.mobility.sfb.msgpack.MessagePackByteArrayMapper;
 import de.fraunhofer.fit.cscw.mobility.sfb.protobuf.ProtobufByteArrayMapper;
 import de.fraunhofer.fit.cscw.mobility.sfb.xml.JacksonXmlStringMapper;
+import de.fraunhofer.fit.cscw.mobility.sfb.xml.JaxbXmlStringMapper;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * @author Sevket Goekay <goekay@dbis.rwth-aachen.de>
@@ -17,6 +22,7 @@ public class SanityChecks {
 
     @Test
     public void testIfEqual() throws Exception {
+        JaxbXmlStringMapper jaxbXmlStringMapper = new JaxbXmlStringMapper(false);
         JacksonXmlStringMapper jacksonXmlMapper = new JacksonXmlStringMapper();
         JacksonJsonStringMapper jacksonJsonMapper = new JacksonJsonStringMapper();
         MessagePackByteArrayMapper messagePackMapper = new MessagePackByteArrayMapper();
@@ -24,12 +30,27 @@ public class SanityChecks {
         ArrayOfBeer groundTruth = Utils.GROUND_TRUTH;
         String xmlFile = Utils.XML_FILE;
 
+        Files.write(Paths.get("target", "data-jaxb.xml"), jaxbXmlStringMapper.writeNoThrow(groundTruth).getBytes(), StandardOpenOption.CREATE);
+        Files.write(Paths.get("target", "data-jackson.xml"), jacksonXmlMapper.writeNoThrow(groundTruth).getBytes(), StandardOpenOption.CREATE);
+        Files.write(Paths.get("target", "data-jackson.json"), jacksonJsonMapper.writeNoThrow(groundTruth).getBytes(), StandardOpenOption.CREATE);
+        Files.write(Paths.get("target", "message-pack"), messagePackMapper.writeNoThrow(groundTruth), StandardOpenOption.CREATE);
+        Files.write(Paths.get("target", "proto-buf"), toProtobuf(groundTruth), StandardOpenOption.CREATE);
+
         Assert.assertEquals(groundTruth, jacksonXmlMapper.read(xmlFile));
         Assert.assertEquals(groundTruth, jacksonJsonMapper.read(jacksonJsonMapper.write(groundTruth)));
         Assert.assertEquals(groundTruth, messagePackMapper.read(messagePackMapper.write(groundTruth)));
-        Assert.assertEquals(groundTruth, ProtobufConverter.INSTANCE.convertBack(
-                ProtobufByteArrayMapper.INSTANCE.read(
-                        ProtobufByteArrayMapper.INSTANCE.write(
-                                ProtobufConverter.INSTANCE.convert(groundTruth)))));
+        Assert.assertEquals(groundTruth, toProtobufAndBack(groundTruth));
     }
+
+    private static byte[] toProtobuf(ArrayOfBeer groundTruth) {
+        return ProtobufByteArrayMapper.INSTANCE.writeNoThrow(
+                ProtobufConverter.INSTANCE.convert(groundTruth));
+    }
+
+    private static ArrayOfBeer toProtobufAndBack(ArrayOfBeer groundTruth) {
+        return ProtobufConverter.INSTANCE.convertBack(
+                ProtobufByteArrayMapper.INSTANCE.readNoThrow(
+                        toProtobuf(groundTruth)));
+    }
+
 }
