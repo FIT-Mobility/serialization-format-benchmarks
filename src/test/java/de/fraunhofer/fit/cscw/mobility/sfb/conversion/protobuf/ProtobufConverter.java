fc3ed8caa33535e855e1e36ce2560dcc4e3622aa
Sevket Goekay <goekay@dbis.rwth-aachen.de>
2018-10-10 15:54:27 +0200

    refactor

--- src/test/java/de/fraunhofer/fit/cscw/mobility/sfb/conversion/protobuf/ProtobufConverter.java
+++ src/test/java/de/fraunhofer/fit/cscw/mobility/sfb/conversion/protobuf/ProtobufConverter.java
@@ -4,9 +4,9 @@ import com.example.myproto.Protobuf;
 import com.example.myschema.ArrayOfBeer;
 import com.example.myschema.BeerType;
 import com.example.myschema.SortType;
-import de.fraunhofer.fit.cscw.mobility.sfb.conversion.Converter;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.Iterables;
+import de.fraunhofer.fit.cscw.mobility.sfb.conversion.Converter;
 
 /**
  * @author Fabian Ohler <fabian.ohler1@rwth-aachen.de>
@@ -21,7 +21,7 @@ public enum ProtobufConverter implements Converter<ArrayOfBeer, Protobuf.ArrayOf
                                        .build();
     }
 
-    public Protobuf.ArrayOfBeerType.BeerType convert(final BeerType beer) {
+    private Protobuf.ArrayOfBeerType.BeerType convert(final BeerType beer) {
         return Protobuf.ArrayOfBeerType.BeerType.newBuilder()
                                                 .setBrand(beer.getBrand())
                                                 .setSort(convert(beer.getSort()))
@@ -30,7 +30,7 @@ public enum ProtobufConverter implements Converter<ArrayOfBeer, Protobuf.ArrayOf
                                                 .build();
     }
 
-    public Protobuf.ArrayOfBeerType.BeerType.SortType convert(final SortType sort) {
+    private Protobuf.ArrayOfBeerType.BeerType.SortType convert(final SortType sort) {
         return Protobuf.ArrayOfBeerType.BeerType.SortType.newBuilder()
                                                          .addAllString(sort.getStrings())
                                                          .build();
@@ -41,14 +41,14 @@ public enum ProtobufConverter implements Converter<ArrayOfBeer, Protobuf.ArrayOf
         return new ArrayOfBeer().withBeers(Collections2.transform(arrayOfBeer.getBeerList(), this::convertBack));
     }
 
-    public BeerType convertBack(final Protobuf.ArrayOfBeerType.BeerType beer) {
+    private BeerType convertBack(final Protobuf.ArrayOfBeerType.BeerType beer) {
         return new BeerType().withBrand(beer.getBrand())
                              .withSort(convertBack(beer.getSort()))
                              .withAlcohol(beer.getAlcohol())
                              .withBrewery(beer.getBrewery());
     }
 
-    public SortType convertBack(final Protobuf.ArrayOfBeerType.BeerType.SortType sort) {
+    private SortType convertBack(final Protobuf.ArrayOfBeerType.BeerType.SortType sort) {
         return new SortType().withStrings(sort.getStringList());
     }
 }
